a2970a09a1017d78427ec8dc172dff6762939b76
Tiago Henriques <trinosauro@gmail.com>
2016-11-13 19:28:53 +0100

    Extract producs into its own JSON file

--- index.js
+++ index.js
@@ -2,39 +2,8 @@ const { send } = require('micro')
 
 // using sample product list from
 // http://json-schema.org/example1.html
-const products = [{
-  "id": 1,
-  "name": "A green door",
-  "price": 12.50,
-  "tags": ["home", "green"]
-}, {
-  "id": 2,
-  "name": "An ice sculpture",
-  "price": 12.50,
-  "tags": ["cold", "ice"],
-  "dimensions": {
-    "length": 7.0,
-    "width": 12.0,
-    "height": 9.5
-  },
-  "warehouseLocation": {
-    "latitude": -78.75,
-    "longitude": 20.4
-  }
-}, {
-  "id": 3,
-  "name": "A blue mouse",
-  "price": 25.50,
-  "dimensions": {
-    "length": 3.1,
-    "width": 1.0,
-    "height": 1.0
-  },
-  "warehouseLocation": {
-    "latitude": 54.4,
-    "longitude": -32.7
-  }
-}]
+
+const products = require('./products.json');
 
 module.exports = async (req, res) => {
   const action = actions[req.method]
