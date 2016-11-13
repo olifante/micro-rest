a39032230763140d5b1aa189e6368713eca2eda2
Tiago Henriques <trinosauro@gmail.com>
2016-11-13 18:33:07 +0100

    Return 404 for PUT, PATCH and DELETE

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const { send } = require( 'micro')
+const { send } = require('micro')
 
 const actions = {
   'GET': doGet,
-  'POST': doPost
+  'POST': doPost,
+  'PUT': doMethodNotFound,
+  'PATCH': doMethodNotFound,
+  'DELETE': doMethodNotFound,
 }
 
 module.exports = async function (req, res) {
   const action = actions[req.method]
 
   if (!action) {
-    return send(res, 500, {'error': 'Only get and post allowed'})
+    return send(res, 500, { 'error': 'Unknown or unsupported HTTP method' })
   }
 
   return action(req, res)
 }
 
 async function doGet(req, res) {
-  return send(res, 200, {'action': 'GET'})
+  return send(res, 200, { 'action': 'GET' })
 }
 
 async function doPost(req, res) {
-  return send(res, 200, {'action': 'POST'})
+  return send(res, 200, { 'action': 'POST' })
+}
+
+async function doMethodNotFound(req, res) {
+  return send(res, 404, { 'notFound': 'Endpoint only accepts GET and POST methods' })
 }
